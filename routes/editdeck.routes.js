236b411dc57a9660465132c02c4807e228841e72
David <drgomez92@gmail.com>
2020-07-06 17:57:06 +0200

    Añadida función de añadir cartas a mazo.

--- routes/editdeck.routes.js
+++ routes/editdeck.routes.js
@@ -4,16 +4,43 @@ const router = express.Router()
 const Set = require('../models/sets.model')
 const Card = require('../models/card.model')
 
-router.post('/:id', (req, res) => {
-    const cardsSelected = req.body.cardname
+router.post('/add/:id', (req, res) => {
+    let cardsSelected = []
+
+    if(typeof(req.body.cardname) === 'string') {
+        cardsSelected.push(req.body.cardname)
+    } else {
+        cardsSelected = req.body.cardname
+    }
 
     cardsSelected.forEach(card => 
         Card
             .findById(card)
             .then(cardGet => Set.findByIdAndUpdate(req.params.id, {$push: {cards: cardGet}}, {new: true}))
             .catch(err => console.log(err))
-            )
+    )
+    res.redirect(`/profile/setedit/${req.params.id}`)
 })
 
+router.post('/deletecard/:id', (req, res) => {
+    let cardsSelected = []
+
+    if(typeof(req.body.cardname) === 'string') {
+        cardsSelected.push(req.body.cardname)
+    } else {
+        cardsSelected = req.body.cardname
+    }
+
+    console.log(req.params.id)
+    console.log(cardsSelected)
+
+    cardsSelected.forEach(card => {
+        console.log(card)
+        Set
+            .findByIdAndUpdate(req.params.id, {$pull: {"cards": {_id: card}}}, {new: true})
+            .catch(err => console.log(err))
+    })
+    res.redirect(`/profile/setedit/${req.params.id}`)
+})
 
 module.exports = router
